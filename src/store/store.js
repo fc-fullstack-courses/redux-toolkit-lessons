82f436809e7524272e9c8c2b098ebf6c935e1c5a
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-07 20:23:12 +0200

    Add counterSlice

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './slices/counterSlice';
 
-const store = configureStore({});
+const store = configureStore({ reducer: counterReducer });
 export default store;
