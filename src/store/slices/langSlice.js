8d729927d3573a7efaf36c530d1d1c1ae83cba02
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-08 21:15:05 +0200

    Add langSlice

---
+++ src/store/slices/langSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+import CONSTANTS from '../../constants';
+
+const { LANGUAGE } = CONSTANTS;
+
+const initialState = LANGUAGE.EN_US;
+
+const langSlice = createSlice({
+  name: 'lang',
+  initialState,
+  reducers: {
+    setLang: (state, action) => action.payload,
+  },
+});
+
+const { reducer, actions } = langSlice;
+const { setLang } = actions;
+
+export { setLang };
+export default reducer;
