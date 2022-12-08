7ae20864f9d4e3396fc79d4aaf3d2a951980bd93
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-08 20:06:35 +0200

    Add input for step

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { increment, decrement } from '../../store/slices/counterSlice';
+import { increment, decrement, setStep } from '../../store/slices/counterSlice';
 
 const Counter = (props) => {
   const { count, step, dispatch } = props;
   return (
     <div>
       <p>Count: {count}</p>
-      <p>Step: {step}</p>
+      <label>
+        Step:{' '}
+        <input
+          type="number"
+          value={step}
+          onChange={({ target: { value } }) => dispatch(setStep(value))}
+        />
+      </label>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
