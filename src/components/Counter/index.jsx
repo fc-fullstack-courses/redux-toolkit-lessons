242bc14e64ee72f565935048adcb3397a7a90112
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-08 21:38:57 +0200

    Add language switch

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,22 +1,61 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { setLang } from '../../store/slices/langSlice';
 import { increment, decrement, setStep } from '../../store/slices/counterSlice';
+import CONSTANTS from '../../constants';
+const {
+  LANGUAGE: { EN_US, UA_UA },
+} = CONSTANTS;
+
+const translations = new Map([
+  [
+    EN_US,
+    {
+      countText: 'Count',
+      stepText: 'Step',
+      incrementText: 'Increment',
+      decrementText: 'Decrement',
+    },
+  ],
+  [
+    UA_UA,
+    {
+      countText: 'Рахунок',
+      stepText: 'Крок',
+      incrementText: 'Збільшити',
+      decrementText: 'Зменшити',
+    },
+  ],
+]);
 
 const Counter = (props) => {
-  const { count, step, increment, decrement, setStep } = props;
+  const { count, step, language, increment, decrement, setStep, setLang } =
+    props;
+
+  const translation = translations.get(language);
+  const { countText, stepText, incrementText, decrementText } = translation;
   return (
     <div>
-      <p>Count: {count}</p>
+      <select
+        value={language}
+        onChange={({ target: { value } }) => setLang(value)}
+      >
+        <option value={EN_US}>English</option>
+        <option value={UA_UA}>Ukrainian</option>
+      </select>
+      <p>
+        {countText}: {count}
+      </p>
       <label>
-        Step:{' '}
+        {stepText}:{' '}
         <input
           type="number"
           value={step}
           onChange={({ target: { value } }) => setStep(value)}
         />
       </label>
-      <button onClick={() => increment()}>Increment</button>
-      <button onClick={() => decrement()}>Decrement</button>
+      <button onClick={() => increment()}>{incrementText}</button>
+      <button onClick={() => decrement()}>{decrementText}</button>
     </div>
   );
 };
@@ -27,6 +66,7 @@ function mapStateToProps(state) {
     // count: state.counter.count,
     // step: state.counter.step,
     ...state.counter,
+    language: state.lang,
   };
 }
 // v1
@@ -44,6 +84,7 @@ const mapDispatchToProps = {
   increment,
   decrement,
   setStep,
+  setLang,
 };
 // const withState = connect(mapStateToProps);
 // const CounterWithState = withState(Counter);
