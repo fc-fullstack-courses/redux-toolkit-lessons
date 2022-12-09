2983b080d83c298d6c9ed09135098d641bdf061b
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-09 19:25:54 +0200

    Add themes enum

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,10 @@ const CONSTANTS = {
     EN_US: { VALUE: 'en_us', OPTION_TEXT: 'English' },
     UA_UA: { VALUE: 'ua_ua', OPTION_TEXT: 'Українська' },
   },
+  THEMES: {
+    DARK: 'dark',
+    LIGHTL: 'light',
+  },
 };
 
 export default CONSTANTS;
