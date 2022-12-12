49823f6ffa66e5c77689307d834a207f7a7964cc
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-12 21:16:51 +0200

    Add axios get example

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,9 @@
+import axios from 'axios';
+
 export async function getUsers() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = await response.json();
+  const { data: users } = await axios.get(
+    'https://jsonplaceholder.typicode.com/users'
+  );
+
   return users;
 }
