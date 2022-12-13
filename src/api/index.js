b80497672615e9bc1da0214529a35ef6d37377f7
Kyrylo <fresh.js.ftd.@gmail.com>
2022-12-13 18:54:53 +0200

    Add registerUser api method

--- src/api/index.js
+++ src/api/index.js
@@ -2,3 +2,6 @@ import axios from 'axios';
 
 export const getUsers = async () =>
   axios.get('http://localhost:5000/api/users');
+
+export const registerUser = async (userData) =>
+  axios.post('http://localhost:5000/api/users/auth/signup', userData);
